72a522abe84a595536db1f40b097fd55b3dcbdad
mimis <2932508344@qq.com>
2022-02-28 15:31:26 +0800

    添加fridens,group服务

---
+++ src/services/friends/boot.go
@@ -0,0 +1,8 @@
+package friends
+
+import "context"
+
+func Boot(ctx context.Context) {
+	// 初始化服务
+	// InitService()
+}
---
+++ src/services/group/boot.go
@@ -0,0 +1,8 @@
+package group
+
+import "context"
+
+func Boot(ctx context.Context) {
+	// 初始化服务
+	// InitService()
+}

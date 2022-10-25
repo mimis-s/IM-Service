9488008ec809b00aa4d4b57342dc4aa004b1c455
zhangbin <zhangbin@joynova.com.cn>
2022-10-25 11:11:15 +0800

    完善客户端连接session

--- src/services/gateway/service/service.go
+++ src/services/gateway/service/service.go
@@ -1,49 +1,9 @@
 package service
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/mimis-s/IM-Service/src/common/commonproto/im_home_proto"
 	"github.com/mimis-s/IM-Service/src/services/gateway/dao"
-	"github.com/mimis-s/IM-Service/src/services/home/api_home"
 	"github.com/mimis-s/golang_tools/net"
-	"github.com/mimis-s/golang_tools/net/clientConn"
 )
-
-// 服务器处理客户端消息的回调函数(现在的简单框架足够了, 但是后面会集成到session里面)
-func (s *Service) HandlerHttpRespone(reqClient *clientConn.ClientMsg) (*clientConn.ClientMsg, error) {
-	if reqClient.Tag == -1 {
-		// 心跳包
-		fmt.Printf("client send heartCheack\n")
-		return &clientConn.ClientMsg{
-			Tag: -1,
-		}, nil
-	}
-
-	// 先不记录client连接
-	req := &api_home.ClientRequestHandleReq{
-		MsgID:   uint32(reqClient.Tag),
-		Payload: reqClient.Msg,
-		Client: &im_home_proto.ClientOnlineInfo{
-			UserID:   1,
-			UserName: "test",
-		},
-	}
-	res := &api_home.ClientRequestHandleRes{}
-
-	res, err := api_home.ClientRequestHandleJson(context.Background(), req)
-	if err != nil {
-		errStr := fmt.Sprintf("Client Request Handle Json[%v] is err:%v", req, err)
-		fmt.Println(errStr)
-		return nil, fmt.Errorf(errStr)
-	}
-
-	return &clientConn.ClientMsg{
-		Tag: int(res.MsgID),
-		Msg: res.Payload,
-	}, nil
-}
 
 var S *Service
 
@@ -63,7 +23,7 @@ func Init(addr, webAddr string) *Service {
 		Dao: d,
 	}
 
-	httpServer := net.InitServer(webAddr, "http", S.HandlerHttpRespone)
+	httpServer := net.InitServer(webAddr, "http", NewSession)
 
 	go func() {
 		err := httpServer.Listen()
--- src/services/gateway/service/session.go
+++ src/services/gateway/service/session.go
@@ -1,3 +1,86 @@
 package service
 
-// session记录连接情况,(比如当前在线人数等)
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/mimis-s/IM-Service/src/common/commonproto/im_home_proto"
+	"github.com/mimis-s/IM-Service/src/services/home/api_home"
+	"github.com/mimis-s/IM-Service/src/services/home/service/seralize"
+	"github.com/mimis-s/golang_tools/net/clientConn"
+)
+
+var cacheClient = sync.Map{}
+
+// session记录每一个客户端的连接情况
+type Session struct {
+	UserID int64
+}
+
+func NewSession(clientConn.ClientConn) clientConn.ClientSession {
+	return &Session{}
+}
+
+func (s *Session) GetClientConn() clientConn.ClientConn {
+	return nil
+}
+
+func (s *Session) ConnectCallBack() {
+
+}
+
+func (s *Session) RequestCallBack(reqClient *clientConn.ClientMsg) (*clientConn.ClientMsg, error) {
+	if reqClient.Tag == -1 {
+		// 心跳包
+		fmt.Printf("client send heartCheack\n")
+		return &clientConn.ClientMsg{
+			Tag: -1,
+		}, nil
+	}
+	// 如果是登录则记录用户ID和conn的映射
+	loginReqID := seralize.GetMsgIdByStruct(im_home_proto.LoginReq{})
+	if uint32(reqClient.Tag) == loginReqID {
+		// 这里比较完善的做法应该是需要存入redis，但是现在可以先存入map里面
+
+		// 判断当前用户是否已经在线，如果在线，则删除之前的保留现在的
+		loginReq := &im_home_proto.LoginReq{}
+		if s.GetClientConn().GetConnType() == clientConn.ClientConn_HTTP_Enum {
+			json.Unmarshal(reqClient.Msg, loginReq)
+		} else if s.GetClientConn().GetConnType() == clientConn.ClientConn_HTTP_Enum {
+			proto.Unmarshal(reqClient.Msg, loginReq)
+		}
+		s.UserID = loginReq.UserID
+		fmt.Printf("用户[%v] IP[%v]登录成功", s.UserID, s.GetClientConn().GetIP())
+		cacheClient.Store(loginReq.UserID, s)
+	}
+
+	// 先不记录client连接
+	req := &api_home.ClientRequestHandleReq{
+		MsgID:   uint32(reqClient.Tag),
+		Payload: reqClient.Msg,
+		Client: &im_home_proto.ClientOnlineInfo{
+			UserID:   1,
+			UserName: "test",
+		},
+	}
+
+	res, err := api_home.ClientRequestHandleJson(context.Background(), req)
+	if err != nil {
+		errStr := fmt.Sprintf("Client Request Handle Json[%v] is err:%v", req, err)
+		fmt.Println(errStr)
+		return nil, fmt.Errorf(errStr)
+	}
+
+	return &clientConn.ClientMsg{
+		Tag: int(res.MsgID),
+		Msg: res.Payload,
+	}, nil
+}
+
+func (s *Session) DisConnectCallBack() {
+	fmt.Printf("用户[%v] IP[%v]断开连接", s.UserID, s.GetClientConn().GetIP())
+	cacheClient.Delete(s.UserID)
+}

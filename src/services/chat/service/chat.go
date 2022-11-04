0554ef5fe9957f3935ae4add566949e552dd6010
zhangbin <zhangbin@joynova.com.cn>
2022-11-04 09:58:13 +0800

    添加relay服,做延时转发

--- src/services/chat/service/chat.go
+++ src/services/chat/service/chat.go
@@ -6,7 +6,11 @@ import (
 	"github.com/mimis-s/IM-Service/src/services/chat/api_chat"
 )
 
+// 服务器要做的是转发和回发
 func (s *Service) ChatSingle(ctx context.Context, req *api_chat.ChatSingleReq, res *api_chat.ChatSingleRes) error {
+
+	// 转发
+
 	res.Data = req.Data
 	return nil
 }

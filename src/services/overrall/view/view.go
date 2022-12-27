cdd983c112e0f7976b395929266400ad545dea2a
zhangbin <zhangbin@joynova.com.cn>
2022-12-27 12:14:17 +0800

    修改雪花算法实例化对象的workerid

--- src/services/overrall/view/view.go
+++ src/services/overrall/view/view.go
@@ -1,18 +1,30 @@
 package view
 
 import (
+	"sync"
 	"time"
 
 	"github.com/yitter/idgenerator-go/idgen"
 )
+
+var singletonMutex sync.Mutex
+
+// 每一个实例的workid都是不一样的
+// 默认workid最大值为2^6-1 -> 63, 意思就是最大可以创建63个雪花算法对象实例, 对于一个服务器来说已经够用了
+var workerId = uint16(1)
 
 type IdgenObject struct {
 	idGenerator *idgen.DefaultIdGenerator
 }
 
 func NewIdgenObject() *IdgenObject {
-	options := idgen.NewIdGeneratorOptions(1)
+	singletonMutex.Lock()
+	defer func() {
+		singletonMutex.Unlock()
+	}()
+	options := idgen.NewIdGeneratorOptions(workerId)
 	idGenerator := idgen.NewDefaultIdGenerator(options)
+	workerId++
 	return &IdgenObject{idGenerator: idGenerator}
 }
 

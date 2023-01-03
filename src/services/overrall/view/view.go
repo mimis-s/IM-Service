994d52ee72649ce1bb3beac59df2d822e2012489
zhangbin <zhangbin@joynova.com.cn>
2023-01-03 10:06:10 +0800

    修改雪花算法的生成id长度

--- src/services/overrall/view/view.go
+++ src/services/overrall/view/view.go
@@ -10,7 +10,7 @@ import (
 var singletonMutex sync.Mutex
 
 // 每一个实例的workid都是不一样的
-// 默认workid最大值为2^6-1 -> 63, 意思就是最大可以创建63个雪花算法对象实例, 对于一个服务器来说已经够用了
+// 默认workid最大值为2^WorkerIdBitLength - 1
 var workerId = uint16(1)
 
 type IdgenObject struct {
@@ -24,6 +24,10 @@ func NewIdgenObject() *IdgenObject {
 	}()
 
 	options := idgen.NewIdGeneratorOptions(workerId)
+	// 刚开始为一个月的时间差
+	options.BaseTime = time.Now().Add(-time.Second*2592000).UnixNano() / 1e6
+	options.WorkerIdBitLength = 4
+	options.SeqBitLength = 3
 	idGenerator := idgen.NewDefaultIdGenerator(options)
 	workerId++
 	return &IdgenObject{idGenerator: idGenerator}
